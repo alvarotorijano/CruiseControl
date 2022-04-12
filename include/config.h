f4ca3733112005becd6db2da09b28c79134c1e43
Alvaro Torijano <alvarotorijano@gmail.com>
2022-04-12 21:41:08 +0200

    Initial commit

---
+++ include/config.h
@@ -0,0 +1,6 @@
+#ifndef CONFIG_H__
+#define CONFIG_H__
+
+#define USB_SERIAL_BAUDRATE 115200
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include "../include/config.h"
+
+
+void setup() {
+  Serial.begin(USB_SERIAL_BAUDRATE);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
